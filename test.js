0c51d6009862d09b39c41589532f4fb9520257a4
jadc <fdakfkgnk@gnaind.sc>
2023-04-20 21:40:46 +0800

    c

--- test.js
+++ test.js
@@ -1,7 +1,16 @@
-var CryptoJS = require("crypto-js");
-var axios = require('axios').default;
-var bip32 = require("bip32");
-var bip39 = require("bip39");
+//var CryptoJS = require("crypto-js");
+//var axios = require('axios').default;
+
+import axios from"axios";
+import CryptoJS from "crypto-js";
+import {BIP32Factory} from "bip32";
+import * as ecc from "tiny-secp256k1"
+
+//const ecc = require('tiny-secp256k1')
+//const {BIP32Factory} = require('bip32')
+
+
+const bip32 = BIP32Factory(ecc)
 
 function sha256(input /** string */) {
   return CryptoJS.SHA256(input).toString().toUpperCase();
@@ -12,7 +21,7 @@ function aes256Encrypt(content /*string*/, secret /*string*/) {
 }
 
 function aes256Decrypt(cipher /** base 64 string */, secret /** string */) {
-  var bytes = CryptoJS.AES.decrypt(cipher, secret);
+  var bytes = CryptoJS.AES.decrypt(atob(cipher), secret);
   return bytes.toString(CryptoJS.enc.Utf8);
 }
 
@@ -24,26 +33,27 @@ var keyid = 1; //which key are we on, rotates every 7 days (or sooner, depending
 
 function getKeyFromSecret(sharedsecret, peerid, id) {
   const derivePath =
-    "m/192'/168'/0'/1'" + "/" + peerid.toString() + "/" + id.toString();
+    "m/192'/168'/0'/1'" + "/" + peerid.toString() + "'/" + id.toString()+"'";
 
   const seed = Buffer.from(sharedsecret, "hex");
+
   const derivedSeed = bip32.fromSeed(seed).derivePath(derivePath).publicKey;
 
-  return derivedSeed.toString("hex");
+  return sha256(derivedSeed.toString("hex"));
 }
 
-function getAdderssFromSecret(sharedsecret, peerid, id) {
+function getAddressFromSecret(sharedsecret, peerid, id) {
     const derivePath =
-    "m/127'/0'/0'/1'" + "/" + peerid.toString() + "/" + id.toString();
+    "m/127'/0'/0'/1'" + "/" + peerid.toString() + "'/" + id.toString()+"'";
 
   const seed = Buffer.from(sharedsecret, "hex");
   const derivedSeed = bip32.fromSeed(seed).derivePath(derivePath).publicKey;
 
-  return derivedSeed.toString("hex");
+  return sha256(derivedSeed.toString("hex"));
 }
 
 function hashMessage(message){
-    bigstring = message.prev+message.address+message.chash+message.timestamp.toString()+message.ttl.toString()+message.nonce.toString()
+    var bigstring = message.prev+message.address+message.chash+message.timestamp.toString()+message.ttl.toString()+message.nonce.toString()
     return sha256(bigstring)
 }
 
@@ -72,18 +82,18 @@ function newMessage(plaintext, sharedsecret, peerid, keyid) {
     var message = {
         hash : '',
         prev : '001A8390C75BB921D4CD3BC51A020838C8938F06094EA85659181981F675125A', //hash of genesis, might change
-        address : getAdderssFromSecret(sharedsecret,peerid,keyid),
+        address : getAddressFromSecret(sharedsecret,peerid,keyid),
         chash : sha256(c),
         timestamp : Math.floor(Date.now() / 1000),
         ttl : 604800,
         nonce : 0,
-        content : c
+        content : btoa(c)
     }
     iteratePOW(message)
     return message
 }
 
-var API_URL = "http://vml1wk063.cse.ust.hk"
+var API_URL = "http://vml1wk063.cse.ust.hk:8080"
 
 /** takes address:string, returns a list of messages */
 async function getMessage(address) {
@@ -93,10 +103,11 @@ async function getMessage(address) {
       return response.data //axios already return JSON. This is a list of messages with this address
     }
   } catch (error) {
-    if (error.response.status==404){
-      //TODO: dont panic if not found
-    }else{
-    console.error(error);
+    if (error.response){
+      if (error.response.status==404){return []}
+      else{
+        console.error(error.response)
+      }
     }
   }
 }
@@ -115,11 +126,34 @@ async function postMessage(message) {
 }
 
 
+console.log(getKeyFromSecret(sharedsecret, 0, 0));
+console.log(getKeyFromSecret(sharedsecret, 1, 0));
+console.log(getKeyFromSecret(sharedsecret, 0, 1));
 
-export function App(){
-  return ""
-}
+console.log(getAddressFromSecret(sharedsecret, 0, 0));
+console.log(getAddressFromSecret(sharedsecret, 1, 0));
+console.log(getAddressFromSecret(sharedsecret, 0, 1));
 
-console.log(getKeyFromSecret(sharedsecret, 0));
-console.log(validPOW({hash:"555"}))
+console.log()
+
 console.log(newMessage("",sharedsecret,peerid,keyid))
+
+console.log()
+
+console.log(aes256Encrypt("testing", sharedsecret))
+console.log(aes256Decrypt("U2FsdGVkX1909Pte89XbubHf2DvgRDgHBF1EaDxcHQI=",sharedsecret))
+
+console.log()
+
+var ms = await getMessage("genesis")
+console.log(ms)
+
+var ms = await getMessage("as")
+console.log(ms)
+
+var m = newMessage("testing", sharedsecret, peerid, keyid)
+console.log(m)
+
+console.log(atob(m.content))
+var res = await postMessage(m)
+console.log(res)
